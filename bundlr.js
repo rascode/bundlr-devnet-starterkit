a78a7aaa4164d4fff7533f9a474ad51de3c87743
Rascode <justin@rascode.dev>
2023-01-03 15:02:02 -0500

    restructured code

--- bundlr.js
+++ bundlr.js
@@ -1,28 +1,48 @@
 import Bundlr from "@bundlr-network/client";
 import { stat, statSync } from "fs";
+import path from 'path';
 
 import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
 
 //global configuration object
 const config = {
-    gateway: {
-        dev: "https://devnet.bundlr.network",
-        main: "https://arweave.net"
+    app:{
+        name: "Bundlr Demo",
+        version: "0.1.1",
+        contentType: "text/html"
+    },
+    env:{
+        dev:{
+            gateway: "https://devnet.bundlr.network",
+            currency: "matic",
+            rpc: "https://rpc-mumbai.maticvigil.com"
+        },
+        prod:
+        {
+            gateway: "https://arweave.net",
+            currency: "matic",
+            rpc: "matic-rpc.com"
+
+        }
     },
     currency: "matic",
     rpc: "https://rpc-mumbai.maticvigil.com/",
     key: process.env.WALLET_KEY,
     default:{
         payload: "hello mastermind",
-        filePath: "./src/assets/demo-file.jpeg",
+        file:{
+            path:"./src/assets/demo-file.jpeg",
+        },
         folderPath: "./src",
-        dataSize: 4_442_424
+        fund:{
+            amount: 4_444_424
+        }
     }
 }
 
 async function getFileSize(filePath){
-    const pathToFile = filePath ? filePath : config.default.filePath
+    const pathToFile = filePath ? filePath : config.default.file.path
     const {size} = statSync(pathToFile)
     return size
 }
@@ -35,7 +55,7 @@ async function getUploadPrice(uploadSize){
 }
 
 // Connect to a Bundlr Node
-const bundlr = new Bundlr.default(config.gateway.dev, config.currency, config.key, { providerUrl: config.rpc});
+const bundlr = new Bundlr.default(config.env.dev.gateway, config.env.dev.currency, config.key, { providerUrl: config.env.dev.rpc});
 await bundlr.ready()
 
 console.log(`Your Bundlr node is funded by the following wallet address: ${bundlr.address} \n`);
@@ -49,19 +69,17 @@ async function getNodeBalance(){
     console.log(`Your bundlr node has a current balance of ${convertedAtomicBalance} ${config.currency}`)
 }
 
-
 // Get Cost estimate in the same currenc
 async function getUploadCostEstimate(filePath){
     // If no value is passed into this function, we default to checking the price to upload 1MB of data
     // The function accepts a number of bytes, so to check the price of 1MB, check the price of 1,048,576 bytes.
 
-    const size = await getFileSize(filePath ? filePath : config.default.filePath)
+    const size = await getFileSize(filePath ? filePath : config.default.file.path)
     const {atomicPrice, convertedPrice} = await getUploadPrice(size)
     
     console.log(`It will run you about ${convertedPrice} to upload ${size} bytes of data. \n`)
     return atomicPrice
 }
-
 
 // Add currency to Bundlr Node
 async function fundBundlrNode(filePath) {
@@ -86,12 +104,29 @@ async function uploadData(payload){
     }
 }
 
-//docs: https://docs.bundlr.network/docs/sdk/Basic%20Features/uploading-file
-async function uploadFile(fileName){
-    const file = fileName ? fileName : config.default.filePath
+async function uploadFile(fileName, contentType, appName){
+    
+    const asset = fileName ? fileName : config.default.file.path
+    const content = contentType ? contentType : config.default.file.type
+    const app = appName ? appName : config.app.name
+    const type = path.extname(config.default.file.path)
+    const tags = [
+        { name: "Content-Type", value: type },
+        { name: "appName", value: config.app.name}
+    ]
+
+    // check upload fees against wallet balance.  Add funds to wallet to conver upload tx
+    if(getUploadCostEstimate(asset) < getNodeBalance){
+        console.log("Need more cash. Executing Fund command... \n")
+        await fundBundlrNode(fileName)
+        console.log("Node now funded for transaction. Initiating Upload now... \n")
+    }else {console.log ("funding good")}
+    
     try {
-        let response = await bundlr.uploadFile(file)
-        console.log(`File uploaded to ${config.gateway.main}/${response.id}`)
+        const tx = await bundlr.uploadFile(asset, {
+            tags: tags
+        })
+        console.log(`File uploaded to ${config.env.prod.gateway}/${tx.id}`)
     } catch (error) {
         console.log("The following mistakes were made:", error)
     }
@@ -112,11 +147,11 @@ async function uploadFolder(folderPath){
     }
 }
 
-await getFileSize()
-await getNodeBalance()
-await getUploadCostEstimate()
-//await fundBundlrNode()
-//await uploadData()
-//await uploadFile()
-await uploadFolder()
+// await getFileSize()
+// await getNodeBalance()
+// await getUploadCostEstimate()
+// await fundBundlrNode()
+// await uploadData()
+await uploadFile()
+// await uploadFolder()
 
