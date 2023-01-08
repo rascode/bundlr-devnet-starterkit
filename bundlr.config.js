cbfef644935bfbca9b05b317a5baf3841b143e05
Rascode <justin@rascode.dev>
2023-01-08 10:15:00 -0500

    reformatted config file.

--- bundlr.config.js
+++ bundlr.config.js
@@ -4,21 +4,28 @@ dotenv.config()
 export default {
     title: "This is my title",
     description: "and my description",
-    wallet:{
+    permaweb_url: "https://arweave.net",
+    wallet: {
         key: process.env.WALLET_KEY
     },
-    app:{
+    app: {
         name: "Bundlr Demo",
         version: "0.1.1",
         contentType: "text/html"
     },
-    env:{
-        dev:{
-            gateway: "https://devnet.bundlr.network",
-            currency: "matic",
-            rpc: "https://rpc-mumbai.maticvigil.com"
+    env: {
+        dev: {
+            permaweb: {
+                gateway: "https://devnet.bundlr.network",
+                url: "https://arweave.net"
+            },
+            net: {
+                currency: "matic",
+                rpc: "https://rpc-mumbai.maticvigil.com",
+                explorer: "https://mumbai.polygonscan.com"
+            }
         },
-        test:{
+        test: {
             gateway: "",
             currency: "",
             rpc: ""
@@ -26,11 +33,14 @@ export default {
         prod:
         {
             gateway: "https://arweave.net",
-            currency: "matic",
-            rpc: "matic-rpc.com"
+            net: {
+                currency: "matic",
+                rpc: "matic-rpc.com",
+                explorer: ""
+            }
         }
     },
-    data:{
+    data: {
         path: "./src/assets/payload.json",
         payload: {
             hello: "mastermind",
@@ -41,29 +51,29 @@ export default {
             remeber: "to reach out for help if you get stuck",
             us: "devs have to stick together",
             and: "remember",
-            no:{
-                person:{
-                    is:{
+            no: {
+                person: {
+                    is: {
                         an: "island"
-                        }
+                    }
                 }
             }
         },
         title: "Demo Bundlr network data set",
         description: "This is an example of what it looks like to upload a data object/payload to the arweave permaweb via the Bundlr Development network",
     },
-    file:{
-        path:"./src/assets/majestic.jpeg",
+    file: {
+        path: "./src/assets/majestic.jpeg",
         title: "Demo Bundlr Network File",
         description: "This is an example of what it looks like to upload a single image to the arweave permaweb using the Bundlr Development network"
     },
-    folder:{
+    folder: {
         path: "./src/assets/collection",
         title: "Demo Bundlr Network Folder",
         description: "This is an example folder upload to the arweave permaweb using the Bundlr Development network",
         contentType: "text/html"
     },
-    fund:{
+    fund: {
         amount: 4_333_222
     }
 }
